035252b3db6b51e862c4c51b13234e454896113c
pqasys <63470796+pqasys@users.noreply.github.com>
2025-08-27 23:31:20 +0100

    Update langcsebkg5.sql

--- langcsebkg5.sql
+++ langcsebkg5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 27, 2025 at 09:22 PM
+-- Generation Time: Aug 27, 2025 at 10:30 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.1.13
 
